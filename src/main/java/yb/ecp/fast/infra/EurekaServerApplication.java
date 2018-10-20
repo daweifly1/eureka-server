540bbfb8bbaaeec71667427ea100944f91835ca9
tigerwer@126.com <wangyidan1>
2018-10-20 11:32:56 +0800

    sas

--- src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
+++ src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
@@ -1,15 +1,11 @@
 package yb.ecp.fast.infra;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
-@EnableAdminServer
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] a) {
         SpringApplication.run(EurekaServerApplication.class, a);
