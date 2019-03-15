58263944834d3185837cc7a45a6e0a78c00fef78
tigerwer@126.com <tigerwer@126.com>
2019-03-15 16:10:30 +0800

    注册中心+配置中心

--- src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
+++ src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
@@ -2,12 +2,13 @@ package yb.ecp.fast.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 //@EnableZipkinServer
-//@EnableConfigServer
+@EnableConfigServer
 //@EnableAdminServer
 public class EurekaServerApplication {
     public static void main(String[] a) {
