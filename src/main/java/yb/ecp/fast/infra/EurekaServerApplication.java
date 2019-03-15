3dc90b5a69593bbb2a88a57fef6335adcd3e8a97
tigerwer@126.com <tigerwer@126.com>
2019-03-15 15:26:33 +0800

    由于yb将配置中心 admin zipkin全部集成一起 /health访问耗时过大，移除其他配置仅保留注册中心

--- src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
+++ src/main/java/yb/ecp/fast/infra/EurekaServerApplication.java
@@ -1,18 +1,14 @@
 package yb.ecp.fast.infra;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableZipkinServer
-@EnableConfigServer
-@EnableAdminServer
+//@EnableZipkinServer
+//@EnableConfigServer
+//@EnableAdminServer
 public class EurekaServerApplication {
     public static void main(String[] a) {
         SpringApplication.run(EurekaServerApplication.class, a);
